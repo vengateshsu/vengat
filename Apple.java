da5d5a7e4f08c73c2f978899f37e78c087e94f8f
vengateshsu <vengatmass97@gmail.com>
2017-04-19 18:44:56 +0530

    Add files via upload

--- Apple.java
+++ Apple.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
-class Apple{
+class Repeat1{
 public static void main(String args[])
 {
 int sum=0;
 int[] x=new int[20];
 Scanner ob1=new Scanner(System.in);
-for(int i=0;i<5;i++){
+int n=ob1.nextInt();
+for(int i=0;i<n;i++){
 x[i]=ob1.nextInt();
 }
-for(int i=0;i<5;i++){
+for(int i=0;i<n;i++){
 sum=sum^x[i];
 }
 System.out.println("The one time repeat element is:"+sum);
